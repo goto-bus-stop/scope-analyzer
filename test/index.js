903c41bcbfa9457a681a26a3d0da24014f1648d4
Renée Kooi <renee@kooi.me>
2017-12-13 16:51:23 +0100

    add test for reference tracking

--- test/index.js
+++ test/index.js
@@ -64,3 +64,47 @@ test('shadowing', function (t) {
   t.ok(fn.has('a'))
   t.notEqual(fn.getBinding('a'), root.getBinding('a'))
 })
+
+test('references', function (t) {
+  t.plan(5)
+
+  var src = `
+    var a = 0
+    a++
+    a++
+    function b (b) {
+      console.log(b(a))
+    }
+    b(function (b) { return a + b })
+  `
+  var ast = crawl(src)
+
+  var root = scan.scope(ast)
+  var fn = scan.scope(ast.body[3])
+  var callback = scan.scope(ast.body[4].expression.arguments[0])
+
+  var a = root.getBinding('a')
+  t.equal(a.getReferences().length, 5)
+  var b = root.getBinding('b')
+  t.equal(b.getReferences().length, 2)
+  var b2 = fn.getBinding('b')
+  t.equal(b2.getReferences().length, 2)
+  var b3 = callback.getBinding('b')
+  t.equal(b3.getReferences().length, 2)
+
+  // try to rewrite some things
+  var result = src.split('')
+  a.getReferences().forEach(function (ref) { result[ref.start] = 'x' })
+  b.getReferences().forEach(function (ref) { result[ref.start] = 'y' })
+  b2.getReferences().forEach(function (ref) { result[ref.start] = 'z' })
+  b3.getReferences().forEach(function (ref) { result[ref.start] = 'w' })
+  t.equal(result.join(''), `
+    var x = 0
+    x++
+    x++
+    function y (z) {
+      console.log(z(x))
+    }
+    y(function (w) { return x + w })
+  `, 'references were associated correctly')
+})
